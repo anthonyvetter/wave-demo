3ec1369f9642c1f03d0cd60950fcf85f4a551633
av <av@iMac.vetter.tech>
2020-06-10 13:51:06 -0700

    fixing build

--- build-scripts/spring-petclinic-build.sh
+++ build-scripts/spring-petclinic-build.sh
@@ -2,3 +2,6 @@
 
 cd spring-petclinic
 # mvn clean package
+mvn -Dmaven.repo.local=$(pwd)/cache -f $(pwd)/source/pom.xml clean install
+find source/target -name *.jar -exec mv "{}" target/spring-petclinic.jar \;
+cp  source/Dockerfile target/Dockerfile
